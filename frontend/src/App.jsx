a25be9b45c005969558201b033ce6d36dd124ae9
Sabeel Hamood <sabeelhamood@example.com>
2025-11-11 15:27:49 +0200

    chore: remove competition gameplay UI screens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/layout/Layout'
 import SimpleQuestionPage from './pages/SimpleQuestionPage'
 import CompetitionPage from './pages/CompetitionPage'
-import CompetitionDetail from './pages/competition/CompetitionDetail'
-import CompetitionQuestionEnhanced from './components/CompetitionQuestionEnhanced'
 import CompetitionInvitation from './components/CompetitionInvitation'
-import CompetitionResultsEnhanced from './components/CompetitionResultsEnhanced'
-import MockCompetitionGameplay from './pages/MockCompetitionGameplay'
 import './styles/theme-transitions.css'
 
 function App() {
@@ -19,10 +15,6 @@ function App() {
           <Route path="/question" element={<SimpleQuestionPage />} />
           <Route path="/competitions" element={<CompetitionPage />} />
           <Route path="/competition/invitation" element={<CompetitionInvitation />} />
-          <Route path="/competition/:id" element={<CompetitionDetail />} />
-          <Route path="/competition/:id/question/:questionId" element={<CompetitionQuestionEnhanced />} />
-          <Route path="/competition/:id/results" element={<CompetitionResultsEnhanced />} />
-          <Route path="/mock-competition/:competitionId/question/:questionId" element={<MockCompetitionGameplay />} />
         </Routes>
       </Layout>
     </Router>
