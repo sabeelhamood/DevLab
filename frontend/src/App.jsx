51407919ce2363e4dda75cf1b6e39d666a5a0a54
Sabeel Hamood <sabeelhamood@example.com>
2025-11-15 22:05:35 +0200

    feat: expose /dashboard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/layout/Layout'
 import SimpleQuestionPage from './pages/SimpleQuestionPage'
+import Dashboard from './pages/learner/Dashboard'
 import './styles/theme-transitions.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SimpleQuestionPage />} />
           <Route path="/question" element={<SimpleQuestionPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Layout>
     </Router>
